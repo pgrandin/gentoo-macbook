ade24e2e6d1b901df96fd2b8061c83198aa77af5
Pierre Grandin <grandinp@gmail.com>
2017-06-20 13:16:04 -0700

    Adding setup script stub

---
+++ files/setup.sh
@@ -0,0 +1,9 @@
+mount -t proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+
+test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
+mount -t tmpfs -o nosuid,nodev,noexec shm /dev/shm
+chmod 1777 /dev/shm
